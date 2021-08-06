797e37f432884fdb6855286bb151e34b6d2c838b
metzkorn <etzkornmichael@gmail.com>
2021-08-06 10:18:36 -0500

    negative

--- 2020/kitsune/rc6/kitsune20.js
+++ 2020/kitsune/rc6/kitsune20.js
@@ -40313,7 +40313,7 @@ es.prototype.start = function() {
             break;
         default:
             this.kb = {
-                TOTAL_METER: 1,/*
+                TOTAL_METER: -1,/*
                 OPPONENT_COUNT: 6,
                 OPPONENT_MAX_SPEED: 7,
                 OPPONENT_MIN_SPEED: 4,
